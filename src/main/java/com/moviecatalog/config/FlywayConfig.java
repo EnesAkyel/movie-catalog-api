6c7ba9d18688b9fae9c46bdaef404545c7d76026
enesakyel <akyelenes@hotmail.com>
2026-07-22 16:16:49 -0500

    make sure flyway  bean is done first

--- src/main/java/com/moviecatalog/config/FlywayConfig.java
+++ src/main/java/com/moviecatalog/config/FlywayConfig.java
@@ -1,6 +1,8 @@
 package com.moviecatalog.config;
 
 import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,5 +19,17 @@ public class FlywayConfig {
                 .load();
         flyway.migrate();
         return flyway;
+    }
+
+    // Spring Boot 4.x removed FlywayAutoConfiguration which previously ensured
+    // entityManagerFactory depended on Flyway. This restores that ordering.
+    @Bean
+    public static BeanFactoryPostProcessor flywayDependencyConfigurer() {
+        return beanFactory -> {
+            if (beanFactory.containsBeanDefinition("entityManagerFactory")) {
+                BeanDefinition bd = beanFactory.getBeanDefinition("entityManagerFactory");
+                bd.setDependsOn("flyway");
+            }
+        };
     }
 }
